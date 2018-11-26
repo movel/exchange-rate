08838529c65e2d1fc43c96efa89cf3811e2c3438
C64\COMP64 <movel22@gmail.com>
2018-11-26 22:27:36 +0300

    add containers and componenets

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import Layout from './hoc/layout/Layout'
 import './App.css';
+import Quiz from './containers/Quiz/Quiz'
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <div>
-          <h1>Layout works</h1>
-        </div>
+        <Quiz />
       </Layout>
     );
   }
