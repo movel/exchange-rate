abdc72d15518ab815ed426fa2bad31ab123587ef
C64\COMP64 <movel22@gmail.com>
2018-12-01 23:21:06 +0300

    add auth form

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,44 @@
 import React, { PureComponent } from 'react';
+import classes from './Auth.module.css'
+import Button from '../../components/UI/Button/Button'
 
 class Auth extends PureComponent {
+  loginHandler = () => {
+
+  }
+
+  registerHandler = () => {
+    
+  }
+
+  submitHandler = event => {
+    event.preventDefault()
+  }
+
   render() {
     return (
-      <div>
-        <h1>Auth</h1>
+      <div className={ classes.Auth }>
+        <div>
+          <h1>Authorisation</h1>
+
+          <form onSubmit={ this.submitHandler } className={ classes.AuthForm }>
+            <input type="text" />
+            <input type="text" />
+
+            <Button 
+              type="success" 
+              onClick={ this.loginHandler}
+            >
+              Log in
+            </Button>
+            <Button 
+              type="primary" 
+              onClick={ this.registerHandler}
+            >
+              Register
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
