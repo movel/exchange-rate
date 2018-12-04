acbbc456875eeae45397019642e6c0c837bcdf5f
C64\COMP64 <movel22@gmail.com>
2018-12-04 20:05:12 +0300

    add validation part 1

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -4,6 +4,36 @@ import Button from '../../components/UI/Button/Button'
 import Input from '../../components/UI/Input/Input'
 
 class Auth extends PureComponent {
+
+  state = {
+    formControls: {
+      email: {
+        value: '',
+        type: 'email',
+        label: 'Email',
+        errorMessage: 'Enter correct email',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true
+        }
+      },
+      password: {
+        value: '',
+        type: 'password',
+        label: 'Password',
+        errorMessage: 'Enter correct password',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6
+        }
+      }
+    }
+  }
+
   loginHandler = () => {
 
   }
@@ -16,6 +46,29 @@ class Auth extends PureComponent {
     event.preventDefault()
   } 
 
+  onChangeHandler = (event, controlName) => {
+    console.log(`${ controlName }: `, event.target.value)
+  }
+
+  renderInputs() {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName]
+      return (
+        <Input 
+          key={ controlName + index }
+          type={ control.type }
+          value={ control.value }
+          valid={ control.valid }
+          touched={ control.touched }
+          label={ control.label }
+          shouldValidate={ !!control.validation }
+          errorMessage={ control.errorMessage }
+          onChange={ event => this.onChangeHandler(event, controlName) }
+        />
+      )
+    })
+  }
+
   render() {
     return (
       <div className={ classes.Auth }>
@@ -23,13 +76,8 @@ class Auth extends PureComponent {
           <h1>Authorisation</h1>
 
           <form onSubmit={ this.submitHandler } className={ classes.AuthForm }>
-            <Input 
-              label="Email" 
-            />
-            <Input 
-              label="Password"
-              errorMessage="TEST!!!"
-            />
+            
+            { this.renderInputs() }
 
             <Button 
               type="success" 
