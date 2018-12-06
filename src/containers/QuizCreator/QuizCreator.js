a3412f7d92c8811f43b9ea0e23b5c69bd4b41473
C64\COMP64 <movel22@gmail.com>
2018-12-06 16:09:36 +0300

    add formFramework

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,8 +1,30 @@
 import React, { PureComponent } from 'react';
 import classes from './QuizCreator.module.css'
 import Button from '../../components/UI/Button/Button'
+import { createControl } from '../../Form/formFramework'
+
+function createOptionControl(number) {
+  return createControl({
+      label: `Option ${number}`,
+      errorMessage: `Option don't have idle`
+    }, {reqiered: true})
+  }
 
 class QuizCreator extends PureComponent {
+
+  state = {
+    quiz: [],
+    formControls: {
+      question: createControl({
+        label: 'Enter question',
+        errorMessage: `Question don't have idle`
+      }, {reqiered: true}),
+      option1: createOptionControl(1),
+      option2: createOptionControl(2),
+      option3: createOptionControl(3),
+      option4: createOptionControl(4)
+    }
+  }
 
   submitHandler = event => {
     event.preventDefault()
