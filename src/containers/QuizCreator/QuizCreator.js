7a99ae6a7bd7133d97676c15bedcc5b8464c7c4a
C64\COMP64 <movel22@gmail.com>
2018-12-06 15:29:41 +0300

    add QuizCreator css

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,10 +1,52 @@
 import React, { PureComponent } from 'react';
+import classes from './QuizCreator.module.css'
+import Button from '../../components/UI/Button/Button'
 
 class QuizCreator extends PureComponent {
+
+  submitHandler = event => {
+    event.preventDefault()
+  }
+
+  addQuestionHandler = () => {
+
+  }
+
+  createQuizHandler = () => {
+
+  }
+
   render() {
     return (
-      <div>
-        <h1>Quiz Creator</h1>
+      <div className={ classes.QuizCreator }>
+        <div>
+          <h1>Creating Test</h1>
+
+          <form onSubmit={ this.submitHandler }>
+
+            <input type="text"/>
+            <hr/>
+            <input type="text"/>
+            <input type="text"/>
+            <input type="text"/>
+            <input type="text"/>
+
+            <select></select>
+
+            <Button
+              type="primary"
+              onClick={ this.addQuestionHandler }
+            >
+              Add question
+            </Button>
+            <Button
+              type="success"
+              onClick={ this.createQuizHandler }
+            >
+              Creat test
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
