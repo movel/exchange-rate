40b3e28fc04eafd33c4102c1903937da79bb529c
C64\COMP64 <movel22@gmail.com>
2018-12-07 18:39:15 +0300

    add formCreatorControls

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,29 +1,35 @@
 import React, { PureComponent } from 'react';
 import classes from './QuizCreator.module.css'
 import Button from '../../components/UI/Button/Button'
+import Input from '../../components/UI/Input/Input'
 import { createControl } from '../../Form/formFramework'
+import Auxiliary from '../../hoc/Auxiliary/Auxiliary'
 
 function createOptionControl(number) {
   return createControl({
-      label: `Option ${number}`,
-      errorMessage: `Option don't have idle`
-    }, {reqiered: true})
+    label: `Option ${number}`,
+    errorMessage: `Option don't have idle`
+  }, {reqiered: true})
+}
+
+function createFormControls() {
+  return {
+    question: createControl({
+      label: 'Enter question',
+      errorMessage: `Question don't have idle`
+    }, {reqiered: true}),
+    option1: createOptionControl(1),
+    option2: createOptionControl(2),
+    option3: createOptionControl(3),
+    option4: createOptionControl(4)
   }
+}
 
 class QuizCreator extends PureComponent {
 
   state = {
     quiz: [],
-    formControls: {
-      question: createControl({
-        label: 'Enter question',
-        errorMessage: `Question don't have idle`
-      }, {reqiered: true}),
-      option1: createOptionControl(1),
-      option2: createOptionControl(2),
-      option3: createOptionControl(3),
-      option4: createOptionControl(4)
-    }
+    formControls: createFormControls()
   }
 
   submitHandler = event => {
@@ -38,6 +44,31 @@ class QuizCreator extends PureComponent {
 
   }
 
+  changeHandler = (value, controlName) => {
+
+  }
+
+  renderControls() {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName]
+
+      return (
+        <Auxiliary key={ index }>
+          <Input 
+            label={ control.label }
+            value={ control.value }
+            valid={ control.valid }
+            shouldValidate={ !!control.validation }
+            touched={ control.touched }
+            errorMessage={ control.Message }
+            onChange={ event => this.changeHandler(event.target.value, controlName)}
+          />
+          { index === 0 ? <hr /> : null }
+        </Auxiliary>
+      )
+    })
+  }
+
   render() {
     return (
       <div className={ classes.QuizCreator }>
@@ -46,12 +77,7 @@ class QuizCreator extends PureComponent {
 
           <form onSubmit={ this.submitHandler }>
 
-            <input type="text"/>
-            <hr/>
-            <input type="text"/>
-            <input type="text"/>
-            <input type="text"/>
-            <input type="text"/>
+            { this.renderControls() }
 
             <select></select>
 
