54a8eae16a176939e85104d77ef81249a53d40ad
C64\COMP64 <movel22@gmail.com>
2018-12-26 23:34:47 +0300

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import Layout from './hoc/Layout/Layout'
+import {Route, Switch} from 'react-router-dom'
+import Quiz from './containers/Quiz/Quiz'
+import QuizList from './containers/QuizList/QuizList'
+import Auth from './containers/Auth/Auth'
+import QuizCreator from './containers/QuizCreator/QuizCreator'
 
 class App extends Component {
 
   render() {
     return (
-      <div>
-        <p>Hello React</p>
-      </div>
+      <Layout>
+        <Switch>
+          <Route path="/auth" component={Auth} />
+          <Route path="/quiz-creator" component={QuizCreator} />
+          <Route path="/quiz/:id" component={Quiz} />
+          <Route path="/" component={QuizList} />
+        </Switch>
+      </Layout>
     );
   }
 }
