ac3b1a16c9351ff9473e2f3923c4253001ce737a
C64\COMP64 <movel22@gmail.com>
2018-12-27 22:39:03 +0300

    add currency from 'https://openexchangerates.org'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import Layout from './hoc/Layout/Layout'
-import {Route, Switch} from 'react-router-dom'
-import Quiz from './containers/Quiz/Quiz'
-import QuizList from './containers/QuizList/QuizList'
-import Auth from './containers/Auth/Auth'
-import QuizCreator from './containers/QuizCreator/QuizCreator'
+import Tickers from './components/Tickers/Tickers'
 
 class App extends Component {
 
   render() {
     return (
-      <Layout>
-        <Switch>
-          <Route path="/auth" component={Auth} />
-          <Route path="/quiz-creator" component={QuizCreator} />
-          <Route path="/quiz/:id" component={Quiz} />
-          <Route path="/" component={QuizList} />
-        </Switch>
-      </Layout>
+      <div className="App">
+        <div className="App-header">
+          <h2>Cryptocurrency Ticker</h2>
+        </div>
+        <Tickers />
+      </div>
     );
   }
 }
---
+++ src/components/Tickers/Tickers.js
@@ -0,0 +1,82 @@
+import React, { PureComponent } from 'react';
+import axios from 'axios'
+import Cryptocurrency from '../Cryptocurrency/Cryptocurrency'
+import classes from './Tickers.module.css'
+
+class Tickers extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+          {
+              id: "bitcoin",
+              name: "Bitcoin",
+              symbol: "BTC",
+              price_usd: "1",
+              percent_change_1h: "0",
+              percent_change_24h: "0",
+              percent_change_7d: "0",
+          },
+          {
+              id: "ethereum",
+              name: "Ethereum",
+              symbol: "ETH",
+              price_usd: "1",
+              percent_change_1h: "0",
+              percent_change_24h: "0",
+              percent_change_7d: "0",
+          },
+          {
+              id: "litecoin",
+              name: "Litecoin",
+              symbol: "LTC",
+              price_usd: "1",
+              percent_change_1h: "0",
+              percent_change_24h: "0",
+              percent_change_7d: "0",
+          }
+      ]
+  };
+  }
+
+  componentDidMount() {
+    this.fetchCryptocurrencyData();
+    this.fetchCurrencyData()
+    this.interval = setInterval(() => this.fetchCryptocurrencyData(), 60 * 1000);
+  }
+
+  fetchCryptocurrencyData() {
+    axios.get("https://api.coinmarketcap.com/v1/ticker/")
+      .then(response => {
+        const wanted = ["bitcoin", "ethereum", "litecoin"];
+        const result = response.data.filter(currency => wanted.includes(currency.id));
+        this.setState({ data: result });
+      })
+      .catch(err => console.log(err));
+    } 
+
+  fetchCurrencyData() {
+    axios.get("https://openexchangerates.org/api/latest.json?app_id=1337cbc5bc7d462bb39d6e89e7bd19b2")
+      .then(response => {
+        // const wanted = ["bitcoin", "ethereum", "litecoin"];
+        // const result = response.data.filter(currency => wanted.includes(currency.id));
+        console.log('currency: ', response)
+        // this.setState({ data: result });
+      })
+      .catch(err => console.log(err));
+    }
+
+  render() {
+    const tickers = this.state.data.map((currency) =>
+      <Cryptocurrency data={currency} key={currency.id} />
+    )
+    return (
+      <div className={classes.tickers__container}>
+        <ul className={classes.tickers}>{tickers}</ul>
+        <p>Information updated every minute courtesy of <a href="http://www.coinmarketcap.com" target="_blank" rel="noopener noreferrer">coinmarketcap.com</a></p>
+      </div>
+    );
+  }
+}
+
+export default Tickers;
