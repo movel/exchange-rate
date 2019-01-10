cf09721d8e1acf951cb634542d111b5b505fe7ce
C64\COMP64 <movel22@gmail.com>
2019-01-10 22:57:27 +0300

    add CryptoCurrencyContainer

--- src/components/Cryptocurrency/Cryptocurrency.js
+++ src/components/Cryptocurrency/Cryptocurrency.js
@@ -1,28 +1,22 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import './Cryptocurrency.css'
 
-class Cryptocurrency extends PureComponent {
-  render() {
-    const {
-      id,
-      name,
-      symbol,
-      price_usd,
-      percent_change_1h,
-      percent_change_24h,
-      percent_change_7d,
-    } = this.props.data;
-    return (
-      <li className={"cryptocurrency " + id}>
-        <p className="cryptocurrency__name">{name} ({symbol})</p>
-        <h1>${ (+price_usd).toFixed(2) }</h1>
-        <p className={percent_change_1h > 0 ? "green" : "red"}>{percent_change_1h}% 1hr</p>
-        <p className={percent_change_24h > 0 ? "green" : "red"}>{percent_change_24h}% 24hrs</p>
-        <p className={percent_change_7d > 0 ? "green" : "red"}>{percent_change_7d}% 7days</p>
-      </li>
-    );
-  }
+const Cryptocurrency = props => {
+  const tickers = props.data.map((currency) =>
+    <li className={"cryptocurrency " + currency.id}>
+      <p className="cryptocurrency__name">{currency.name} ({currency.symbol})</p>
+      <h1>${ (+currency.price_usd).toFixed(2) }</h1>
+      <p className={currency.percent_change_1h > 0 ? "green" : "red"}>{currency.percent_change_1h}% 1hr</p>
+      <p className={currency.percent_change_24h > 0 ? "green" : "red"}>{currency.percent_change_24h}% 24hrs</p>
+      <p className={currency.percent_change_7d > 0 ? "green" : "red"}>{currency.percent_change_7d}% 7days</p>
+    </li>
+  )
+    
+  return (
+    <ul className="tickers">
+      { tickers }
+    </ul> 
+  )
 }
 
-
-export default Cryptocurrency;
+export default Cryptocurrency
--- src/components/Tickers/Tickers.js
+++ src/components/Tickers/Tickers.js
@@ -1,7 +1,6 @@
 import React, { PureComponent } from 'react'
-import axios from 'axios'
 import Select from 'react-select'
-import Cryptocurrency from '../Cryptocurrency/Cryptocurrency'
+import CryptoCurrencyContainer from '../../containers/CryptoCurrencyContainer/CryptoCurrencyContainer'
 import CurrencyContainer from '../../containers/CurrenciesContainer/CurrenciesContainer'
 import classes from './Tickers.module.css'
 import * as constants from '../../options'
@@ -10,56 +9,12 @@ class Tickers extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      data: [
-          {
-              id: "bitcoin",
-              name: "Bitcoin",
-              symbol: "BTC",
-              price_usd: "1",
-              percent_change_1h: "0",
-              percent_change_24h: "0",
-              percent_change_7d: "0",
-          },
-          {
-              id: "ethereum",
-              name: "Ethereum",
-              symbol: "ETH",
-              price_usd: "1",
-              percent_change_1h: "0",
-              percent_change_24h: "0",
-              percent_change_7d: "0",
-          },
-          {
-              id: "litecoin",
-              name: "Litecoin",
-              symbol: "LTC",
-              price_usd: "1",
-              percent_change_1h: "0",
-              percent_change_24h: "0",
-              percent_change_7d: "0",
-          }
-      ],
       selectedOption: [{
         "value": "JPY",
         "label": "JPY"
       }],
-  }
-  }
-
-  componentDidMount() {
-    this.fetchCryptocurrencyData()
-    // this.interval = setInterval(() => this.fetchCryptocurrencyData(), 60 * 1000);
-  }
-
-  fetchCryptocurrencyData() {
-    axios.get("https://api.coinmarketcap.com/v1/ticker/")
-      .then(response => {
-        const wanted = ["bitcoin", "ethereum", "litecoin"]
-        const data = response.data.filter(currency => wanted.includes(currency.id))
-        this.setState({ data })
-      })
-      .catch(err => console.log(err))
     }
+  }
 
   handleChange = (selectedOption) => {
     this.setState({ selectedOption })
@@ -68,13 +23,9 @@ class Tickers extends PureComponent {
   render() {
     const { selectedOption } = this.state
 
-    const tickers = this.state.data.map((currency) =>
-      <Cryptocurrency data={currency} key={currency.id} />
-    )
-
     return (
       <div className={ classes.tickers__container }>
-        <ul className={ classes.tickers }>{ tickers }</ul>
+        <CryptoCurrencyContainer />
         <br />
         <CurrencyContainer selected={selectedOption} />
         <div className={ classes.select }>
