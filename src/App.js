a754770a6a983441fe25d3273044389ec9620854
C64\COMP64 <movel22@gmail.com>
2019-03-01 22:54:59 +0300

    add prop-types

--- src/App.js
+++ src/App.js
@@ -1,59 +1,134 @@
-import React, { Component } from 'react'
-import {NavLink, Route, Switch, Redirect} from 'react-router-dom'
-import { User } from './User'
-import './App.css'
+
+import React, { Component } from 'react';
+import './App.css';
+
+import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
 
 class App extends Component {
-  state = {
-    name: null,
-    imgUrl: null,
-  }
   componentDidMount() {
-    const _onInit = auth2 => {
-      console.log('init OK', auth2)
+    const appId = process.env.REACT_APP_FACEBOOK_CLIENT_ID
+
+    window.fbAsyncInit = () => {
+      window.FB.init({
+        appId: { appId },
+        cookie: true,  // enable cookies to allow the server to access
+                          // the session
+        xfbml: true,  // parse social plugins on this page
+        version: 'v3.2' // use version 2.1
+      });
+  
+      // Now that we've initialized the JavaScript SDK, we call
+      // FB.getLoginStatus().  This function gets the state of the
+      // person visiting this page and can return one of three states to
+      // the callback you provide.  They can be:
+      //
+      // 1. Logged into your app ('connected')
+      // 2. Logged into Facebook, but not your app ('not_authorized')
+      // 3. Not logged into Facebook and can't tell if they are logged into
+      //    your app or not.
+      //
+      // These three cases are handled in the callback function.
+      window.FB.getLoginStatus((response) => {
+        this.statusChangeCallback(response);
+      });
+    };
+  
+    // Load the SDK asynchronously
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }
+  
+  // Here we run a very simple test of the Graph API after login is
+  // successful.  See statusChangeCallback() for when this call is made.
+  testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    window.FB.api('/me', (response) => {
+    console.log('Successful login for: ' + response.name);
+    document.getElementById('status').innerHTML =
+      'Thanks for logging in, ' + response.name + '!';
+    });
+  }
+  
+  // This is called with the results from from FB.getLoginStatus().
+  statusChangeCallback = (response) => {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      this.testAPI();
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      document.getElementById('status').innerHTML = 'Please log ' +
+      'into Facebook.';
     }
-    const _onError = err => {
-      console.log('error', err)
-    }
-    window.gapi.load('auth2', function() {
-      window.gapi.auth2
-        .init({
-          client_id: process.env.REACT_APP_GOOGLE_CLIENT_ID,
-        })
-        .then(_onInit, _onError)
-    })
   }
-  signIn = () => {
-    const auth2 = window.gapi.auth2.getAuthInstance()
-    auth2.signIn().then(googleUser => {
-      const profile = googleUser.getBasicProfile()
-      this.setState({
-        name: profile.getName(),
-        imgUrl: profile.getImageUrl(),
-      })
-    })
+  
+  // This function is called when someone finishes with the Login
+  // Button.  See the onlogin handler attached to it in the sample
+  // code below.
+  checkLoginState = () => {
+    window.FB.getLoginStatus((response) => {
+      this.statusChangeCallback(response);
+    });
   }
-  signOut = () => {
-    const auth2 = window.gapi.auth2.getAuthInstance()
-    auth2.signOut().then(() => {
-      this.setState({
-        name: null,
-        imgUrl: null,
-      })
-    })
+  
+  handleClick = () => {
+    window.FB.login(this.checkLoginState());
   }
+
   render() {
-    const { name, imgUrl } = this.state
+    const appId = process.env.REACT_APP_FACEBOOK_CLIENT_ID
+    
+    const responseFacebook = (response) => {
+      console.log(response);
+    }
+
+    const responseGoogle = (response) => {
+      console.log(response);
+    }
+
     return (
       <div className="App">
-        <header className="App-header">
-          {!name && <button onClick={this.signIn}>Log in</button>}
-          {!!name && <button onClick={this.signOut}>Log out</button>}
-          {!!name && <User name={name} imgUrl={imgUrl} />}
-        </header>
+        <h1>LOGIN WITH FACEBOOK AND GOOGLE</h1>
+
+        <FacebookLogin
+          appId={ appId } //APP ID NOT CREATED YET
+          fields="name,email,picture"
+          callback={responseFacebook}
+        />
+        <br />
+        <br />
+
+
+        <GoogleLogin
+          clientId="" //CLIENTID NOT CREATED YET
+          buttonText="LOGIN WITH GOOGLE"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+        />
+
+        <br />
+        <br />
+        <a href="#main" onClick={this.handleClick}>Login</a>
+
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
