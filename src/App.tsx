330ceb6507c3f775b5ce33661884a38aed7dd298
C64\COMP64 <movel22@gmail.com>
2019-05-22 21:39:18 +0300

    eslint-config-react-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const App: React.FC = () => {
+const App = () => {
+  const [count, setCount] = useState(5)
+
+  useEffect(
+    () => {
+      console.log(count)
+    }, [count]
+  ) 
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
