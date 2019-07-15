de96ae41f8e894090c325b2d6d09a2b95f00dc0a
C64\COMP64 <movel22@gmail.com>
2019-07-15 23:10:15 +0300

    adding Hooks

---
+++ src/containers/CurrenciesContainer/CurrenciesContainer.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Currency from '../../components/Currency/Currency'
+import './CurrenciesContainer.sass'
+
+const CurrenciesContainer = () => {
+  const [dataCurrency, setDataCurrency] = useState({USDJPY: 108.45183333,})
+
+  const [dataDescription, setDataDescription] = useState({"JPY": '',})
+
+  useEffect(() => {
+    fetchCurrencyData()
+  })
+
+  const fetchCurrencyData = () => {
+    const APP_ID = process.env.REACT_API_CURRENCY_LAYER_COM
+
+    let apiTimeSeries = `http://apilayer.net/api/live?access_key=${APP_ID}`
+    apiTimeSeries += '&source=USD&format=1'
+
+    axios.get(apiTimeSeries.trim())
+      .then(response => {
+        const dataCurrency = response.data.quotes
+        setDataCurrency(dataCurrency)
+      })
+      .catch(err => console.log(err))
+  }
+
+  const fetchCurrenciesDescription = () => {
+    const api = 'https://openexchangerates.org/api/currencies.json'
+
+    axios.get(api)
+      .then(response => {
+        const dataDescription = response.data
+        setDataDescription(dataDescription)
+      })
+      .catch(err => console.log(err))
+  }
+
+  return (<Currency selected={selected} state={this.state} />)
+}
+
+export default CurrenciesContainer
