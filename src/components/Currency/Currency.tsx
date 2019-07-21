dcbc41cabc947467723099ef524410dbaa6ad58a
C64\COMP64 <movel22@gmail.com>
2019-07-21 22:36:36 +0300

    adding fetch currency exchange

--- src/components/Currency/Currency.tsx
+++ src/components/Currency/Currency.tsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import shortid from 'shortid'
 import { dataCurrencyContext } from '../../containers/CurrenciesContainer/CurrenciesContainer'
 import './Currency.sass'
 import * as constants from '../../options'
+import { selectedContext } from '../../pages/Tickers/Tickers';
 
-const Currency = (props: { selected: any; }) => {
-  const {
-    selected
-  } = props.selected
+const Currency = () => {
+  const selected = useContext(selectedContext)
+
+  const dataCurrency: any = useContext(dataCurrencyContext)
 
   // const flagCountry = '<img src={`https://www.countryflags.io/${constants.countries[currency.label]}/shiny/64.png`} alt="flag"></img>'
 
-  const tickersCurrency: any = selected.map( (currency: { title: string | undefined; label: string | any; }) =>
+  const tickersCurrency: any = selected.map( (currency: { title: string | undefined; label: string | any; value: string | any; }) =>
     {
-      const flagName = String(constants.countries[currency.label]).toLowerCase()
-      const imgFlag = <img src={`http://flagpedia.net/data/flags/small/${flagName}.png`} alt="flag" />
+      let a: {value: String, label: String} = { value: '', label: ''}
+      let b = constants.countries.find(elem => {
+        return elem.value === currency.value
+      })
+
+      if(b !== undefined) a = b
+        
+      const flagName = String(a.label).toLowerCase()
+      
+      let imgFlag
+      
+      if(flagName === 'eu') imgFlag = <img src="/img/eu.jpg" alt="flag" />
+        else imgFlag = <img src={`http://flagpedia.net/data/flags/small/${flagName}.png`} alt="flag" />
+      
+
       return (
         
             
@@ -24,13 +38,13 @@ const Currency = (props: { selected: any; }) => {
                     imgFlag
                   }
                 </div>
-                {/* <p className="currency__name">
+                <p className="currency__name">
                   { currency.label } - { (+dataCurrency[`USD${currency.label}`]).toFixed(2) }
-                </p> */}
+                </p>
               </li>
             
       )
-    } 
+    }
   )
 
   return (
--- src/containers/CurrenciesContainer/CurrenciesContainer.tsx
+++ src/containers/CurrenciesContainer/CurrenciesContainer.tsx
@@ -2,34 +2,35 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Currency from '../../components/Currency/Currency'
 import './CurrenciesContainer.sass'
-import { selectedContext } from '../../pages/Tickers/Tickers'
 import { createContext } from 'react'
-import * as constants from '../../options'
+import { REACT_API_CURRENCY_LAYER_COM } from '../../env.local'
 
-import shortid from 'shortid'
-
-const dataCurrencyContext = createContext([])
+const dataCurrencyContext = createContext({})
 
 const CurrenciesContainer = () => {
-  // const [dataCurrency, setDataCurrency] = useState({USDJPY: 108.45183333,})
+  const [dataCurrency, setDataCurrency] = useState({USDJPY: 108.45183333,})
 
-  // useEffect(() => {
-  //   fetchCurrencyData()
-  // })
+  useEffect(() => {
+    fetchCurrencyData()
+  },[])
 
-  // const fetchCurrencyData = () => {
-  //   const APP_ID = process.env.REACT_API_CURRENCY_LAYER_COM
+  const fetchCurrencyData = () => {
+    const APP_ID = REACT_API_CURRENCY_LAYER_COM
 
-  //   let apiTimeSeries = `http://apilayer.net/api/live?access_key=${APP_ID}`
-  //   apiTimeSeries += '&source=USD&format=1'
+    console.log('APP_ID', APP_ID)
 
-  //   axios.get(apiTimeSeries.trim())
-  //     .then(response => {
-  //       const dataCurrency = response.data.quotes
-  //       setDataCurrency(dataCurrency)
-  //     })
-  //     .catch(err => console.log(err))
-  // }
+    let apiTimeSeries = `http://apilayer.net/api/live?access_key=${APP_ID}`
+    apiTimeSeries += '&source=USD&format=1'
+
+    axios.get(apiTimeSeries.trim())
+      .then(response => {
+        const dataCurrency = response.data.quotes
+        setDataCurrency(dataCurrency)
+      })
+      .catch(err => console.log(err))
+  }
+
+  console.log('dataCurrency', dataCurrency)
 
   // const fetchCurrenciesDescription = () => {
   //   const api = 'https://openexchangerates.org/api/currencies.json'
@@ -43,41 +44,9 @@ const CurrenciesContainer = () => {
   // }
 
   return (
-        <selectedContext.Consumer>
-          {selected => (
-            // <Currency selected={selected} />
-            selected.map( (currency: { title: string | undefined; label: string | any; value: string | any; }) =>
-              {
-                
-
-                let a: {value: String, label: String} = { value: '', label: ''}
-                let b = constants.countries.find(elem => {
-                  return elem.value === currency.value
-                })
-
-                if(b !== undefined) a = b
-                  
-                const flagName = String(a.label).toLowerCase()
-                
-                const imgFlag = <img src={`http://flagpedia.net/data/flags/small/${flagName}.png`} alt="flag" />
-                return (
-                  
-                      
-                        <li className="currency" key={ shortid.generate() } title={ currency.title }>
-                          <div className="currency__img">
-                            { 
-                              imgFlag
-                            }
-                          </div>
-                          {/* <p className="currency__name">
-                            { currency.label } - { (+dataCurrency[`USD${currency.label}`]).toFixed(2) }
-                          </p> */}
-                        </li>
-                      
-                )
-              }
-          ))}
-        </selectedContext.Consumer>
+    <dataCurrencyContext.Provider value={dataCurrency}>
+      <Currency />
+    </dataCurrencyContext.Provider>
   )
 }
 
